d8880e66eb091565dab82a091381d1047c283a9e
Sungjun <60735372+sungjunleeee@users.noreply.github.com>
2023-01-19 16:00:14 -0500

    Implemented dictionary methods

--- dict/dict.go
+++ dict/dict.go
@@ -7,6 +7,7 @@ import "errors"
 type Dictionary map[string]string
 
 var errNotFound = errors.New("Not found")
+var errWordExists = errors.New("That word already exists")
 
 // Search returns the definition of a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -15,4 +16,14 @@ func (d Dictionary) Search(word string) (string, error) {
 		return "", errNotFound
 	}
 	return definition, nil
+}
+
+// Add adds a word and its definition to the dictionary
+func (d Dictionary) Add(word string, def string) error {
+	_, err := d.Search(word)
+	if err == errNotFound {
+		d[word] = def
+		return nil
+	}
+	return errWordExists
 }
--- main.go
+++ main.go
@@ -8,11 +8,19 @@ import (
 
 func main() {
 	// dict is not a reserved keyword
-	dictionary := dict.Dictionary{"first": "First word"}
-	definition, err := dictionary.Search("first")
+	dictionary := dict.Dictionary{}
+	word := "hello"
+	definition := "Greeting"
+	err := dictionary.Add(word, definition)
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
 	}
+	hello, _ := dictionary.Search(word)
+	fmt.Println("Found:", word, "Definition:", hello)
+	// generating error
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+
 }
