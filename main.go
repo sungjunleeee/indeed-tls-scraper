1ed654955a2278402bdecf8e591c2dedaaef3e63
Sungjun <60735372+sungjunleeee@users.noreply.github.com>
2023-01-19 10:31:55 -0500

    how to make pointers in go

--- main.go
+++ main.go
@@ -1,30 +1,10 @@
 package main
 
-import (
-	"fmt"
-)
-
-func canIDrink(age int) bool {
-	// you can also use initialization statement in switch like if else statement
-	// one way of using switch
-	switch age {
-	case 10:
-		return false
-	case 18:
-		return true
-	}
-	// another way of using switch
-	switch {
-	case age < 18:
-		return false
-	case age == 18:
-		return true
-	case age > 50:
-		return false
-	}
-	return false
-}
+import "fmt"
 
 func main() {
-	fmt.Println(canIDrink(16))
+	a := 2
+	b := &a
+	*b = 20
+	fmt.Println(a)
 }
