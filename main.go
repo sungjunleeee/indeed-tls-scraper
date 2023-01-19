0124fbc613530eb7f8d16d4c4e8311a7bd338630
Sungjun <60735372+sungjunleeee@users.noreply.github.com>
2023-01-19 18:54:11 -0500

    Introducing go routines

--- main.go
+++ main.go
@@ -1,50 +1,18 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
-var errRequestFailed = errors.New("Request failed")
-
 func main() {
-	// you should put {} or make() to initialize map
-	results := map[string]string{}
-	// this is a slice of strings
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
-	}
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-	// the map will be printed in random order
-	// developers designed that way so that ppl don't rely on the order
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
+	go sickCount("Jun")
+	sickCount("Tim")
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking:", url)
-	resp, err := http.Get(url)
-	// if there's no error but the status code is >= 400
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
+func sickCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is super sick 🥵", i)
+		time.Sleep(time.Second)
 	}
-	return nil
 }
