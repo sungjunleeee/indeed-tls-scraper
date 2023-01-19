e66c608e8dd861f7dd2c6eef54cb2ca60b4c203c
Sungjun <60735372+sungjunleeee@users.noreply.github.com>
2023-01-19 14:40:16 -0500

    Implemented internal stringify for struct

--- main.go
+++ main.go
@@ -9,11 +9,5 @@ import (
 func main() {
 	account := accounts.NewAccount("jun")
 	account.Deposit(500)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(600)
-	if err != nil {
-		fmt.Println(err)
-		// log.Fatalln(err) // fmt.Println(err) + os.Exit(1)
-	}
-	fmt.Println(account.Balance())
+	fmt.Println(account)
 }
