3e6c323307238a52ea3413335e16bf30ce0ca4bf
Sungjun <60735372+sungjunleeee@users.noreply.github.com>
2023-01-19 16:31:14 -0500

    Implemented update and delete

--- dict/dict.go
+++ dict/dict.go
@@ -4,10 +4,16 @@ import "errors"
 
 // Dictionary type (alias of type map[string]string)
 // type can have methods in Go!
+// by default, hashmap in Go is a reference type
+// so you don't need to pass a pointer like (d *Dictionary)
 type Dictionary map[string]string
 
-var errNotFound = errors.New("Not found")
-var errWordExists = errors.New("That word already exists")
+var (
+	errNotFound   = errors.New("Not found")
+	errCantUpdate = errors.New("Can't update non-existing word")
+	errCantDelete = errors.New("Can't delete non-existing word")
+	errWordExists = errors.New("That word already exists")
+)
 
 // Search returns the definition of a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -26,4 +32,26 @@ func (d Dictionary) Add(word string, def string) error {
 		return nil
 	}
 	return errWordExists
+}
+
+// Update a word
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	if err == errNotFound {
+		return errCantUpdate
+	}
+	d[word] = definition
+	return nil
+}
+
+// Delete a word
+func (d Dictionary) Delete(word string) error {
+	// delete doesn't return anything and does nothing if the word doesn't exist
+	// which is why we need to check it if we want to know if it was deleted
+	_, err := d.Search(word)
+	if err == nil {
+		delete(d, word)
+		return nil
+	}
+	return errCantDelete
 }
--- main.go
+++ main.go
@@ -7,20 +7,19 @@ import (
 )
 
 func main() {
-	// dict is not a reserved keyword
 	dictionary := dict.Dictionary{}
-	word := "hello"
-	definition := "Greeting"
-	err := dictionary.Add(word, definition)
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	dictionary.Search(baseWord)
+	dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println(word)
 	}
-	hello, _ := dictionary.Search(word)
-	fmt.Println("Found:", word, "Definition:", hello)
-	// generating error
-	err2 := dictionary.Add(word, definition)
-	if err2 != nil {
-		fmt.Println(err2)
-	}
-
+	// Interestingly enough
+	// a short variable declaration can legal
+	// only when you declare multi-varible short declaration (at least one varible should be new)
+	err = dictionary.Delete(baseWord)
 }
