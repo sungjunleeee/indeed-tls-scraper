30c67c13f9b7e2fe92da6031e51dc1acdf31007d
Sungjun <60735372+sungjunleeee@users.noreply.github.com>
2023-01-19 10:08:40 -0500

    Implemented for loop

--- main.go
+++ main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func lenAndUpper(name string) (length int, uppercase string) {
-	defer fmt.Println("I'm done") // defer is executed after the function is done
-	length = len(name)
-	uppercase = strings.ToUpper(name)
-	return // you still need return keyword
+func superAdd(numbers ...int) int {
+	total := 0
+	// one way of doing for loop
+	for i := 0; i < len(numbers); i++ {
+		fmt.Println(numbers[i])
+	}
+	// another way of doing for loop
+	for _, number := range numbers { // you can simply use number instead of _, number
+		total += number
+	}
+	return total
 }
 
 func main() {
-	totalLength, _ := lenAndUpper("nico")
-	fmt.Println(totalLength)
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 }
