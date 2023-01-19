67e9eafbc5b3b65e8dea7bccd96441eb0a671266
Sungjun <60735372+sungjunleeee@users.noreply.github.com>
2023-01-19 10:36:46 -0500

    how to use array and slice

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	a := 2
-	b := &a
-	*b = 20
-	fmt.Println(a)
+	array := [5]string{"nico", "lynn", "dal"}
+	slice := []string{"nico", "lynn", "dal"}
+	slice = append(slice, "flynn") // new slice is returned from append
+	fmt.Println(slice)
+	fmt.Println(array)
 }
