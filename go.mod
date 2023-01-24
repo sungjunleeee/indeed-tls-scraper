70e64d06f74fbed7a42309bb802938176069dce0
Sungjun <60735372+sungjunleeee@users.noreply.github.com>
2023-01-23 21:55:44 -0500

    Complete version of indeed.com scraper (w/ TLS)

--- go.mod
+++ go.mod
@@ -2,18 +2,26 @@ module github.com/sungjunleeee/learngo
 
 go 1.19
 
-require github.com/PuerkitoBio/goquery v1.8.0
+require (
+	github.com/Danny-Dasilva/CycleTLS/cycletls v0.0.0-20220620102923-c84d740b4757
+	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/labstack/echo v3.3.10+incompatible
+)
 
 require (
-	github.com/Danny-Dasilva/CycleTLS/cycletls v0.0.0-20220620102923-c84d740b4757 // indirect
 	github.com/Danny-Dasilva/fhttp v0.0.0-20220524230104-f801520157d6 // indirect
 	github.com/Danny-Dasilva/utls v0.0.0-20220604023528-30cb107b834e // indirect
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/dsnet/compress v0.0.1 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
-	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
-	golang.org/x/net v0.0.0-20220615171555-694bf12d69de // indirect
-	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
-	golang.org/x/text v0.3.7 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.2.0 // indirect
+	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,148 +1,32 @@
 package main
 
 import (
-	"encoding/csv"
-	"fmt"
-	"log"
 	"os"
 	"strings"
 
-	"github.com/Danny-Dasilva/CycleTLS/cycletls"
-	"github.com/PuerkitoBio/goquery"
+	"github.com/labstack/echo"
+	"github.com/sungjunleeee/learngo/scraper"
 )
 
-var baseURL string = "https://www.indeed.com/jobs?q=python&start="
-
-const (
-	userAgent string = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0"
-	ja3       string = "771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-51-57-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25-256-257,0"
-)
-
-type jobDetails struct {
-	id       string
-	title    string
-	company  string
-	location string
-	salary   string
-	summary  string
-	link     string
-}
+const filename string = "jobs.csv"
 
 func main() {
-	totalPages := getPages(baseURL)
-	c := make(chan []jobDetails)
-	for i := 0; i < totalPages; i++ {
-		go getPage(i, c)
-	}
-	file, err := os.Create("jobs.csv")
-	w := csv.NewWriter(file)
-	defer w.Flush()
-	headers := []string{"Link", "Title", "Location", "Salary", "Summary"}
-	err = w.Write(headers)
-	checkErr(err)
+	e := echo.New()
 
-	checkErr(err)
-	jobCounter := 0
-	for i := 0; i < totalPages; i++ {
-		jobs := <-c
-		jobCounter += len(jobs)
-		writeCSV(jobs, w)
-	}
-	fmt.Println("Total", jobCounter, "job(s) exported")
+	// Routes
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
 }
 
-func getPage(pageNum int, mainChan chan<- []jobDetails) {
-	c := make(chan jobDetails)
-	pageURL := baseURL + fmt.Sprint(pageNum*10)
-	fmt.Println("Requesting", pageURL)
-	resp, err := getRespWithTLS()
-	checkErr(err)
-	checkCode(resp.Status)
-	body := strings.NewReader(resp.Body)
-	doc, err := goquery.NewDocumentFromReader(body)
-	checkErr(err)
-
-	jobs := []jobDetails{}
-	blocks := doc.Find(".jobsearch-LeftPane").Find(".job_seen_beacon")
-	blocks.Each(func(i int, s *goquery.Selection) {
-		go extractJob(s, c)
-	})
-	for i := 0; i < blocks.Length(); i++ {
-		jobs = append(jobs, <-c)
-	}
-	mainChan <- jobs
-}
-
-// Request page with get method with tls credentials to avoid 403 error
-func getPages(url string) int {
-	pages := 0
-	resp, err := getRespWithTLS()
-	checkErr(err)
-	checkCode(resp.Status)
-	body := strings.NewReader(resp.Body)
-	doc, err := goquery.NewDocumentFromReader(body)
-	checkErr(err)
-	pages = doc.Find(`nav[role="navigation"]`).Find("a").Length()
-	return pages
-}
-
-// How to avoid request failed with status code: 403
-func getRespWithTLS() (*cycletls.Response, error) {
-	client := cycletls.Init()
-	res, err := client.Do(baseURL, cycletls.Options{
-		Body:      "",
-		Ja3:       ja3,
-		UserAgent: userAgent,
-	}, "GET")
-	// returning the pointer helps avoid copying the whole struct
-	return &res, err
-}
-
-func extractJob(s *goquery.Selection, c chan<- jobDetails) {
-	id, _ := s.Find(".jobTitle>a").Attr("id")
-	id = strings.Split(id, "_")[1]
-	title := s.Find(".jobTitle>a").Text()
-	// link, _ := s.Find(".jobTitle>a").Attr("href")
-	company := s.Find(".companyName").Text()
-	location := s.Find(".companyLocation").Text()
-	salary := s.Find(`.metadata[class*="salary"]`).Text()
-	if salary == "" {
-		salary = "Not Specified"
-	}
-	summary := s.Find(".job-snippet>ul").Text()
-	c <- jobDetails{
-		id:       id,
-		title:    cleanString(title),
-		company:  cleanString(company),
-		location: cleanString(location),
-		salary:   cleanString(salary),
-		summary:  cleanString(summary),
-		link:     "https://www.indeed.com/viewjob?jk=" + id,
-	}
-}
-
-func writeCSV(jobs []jobDetails, w *csv.Writer) {
-	for _, job := range jobs {
-		jobSlice := []string{job.link, job.title, job.location, job.salary, job.summary}
-		err := w.Write(jobSlice)
-		checkErr(err)
-	}
-}
-
-func cleanString(str string) string {
-	// TrimSpace removes leading and trailing spaces
-	// Join and Split removes duplicate spaces
-	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
-}
-
-func checkErr(err error) {
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
-
-func checkCode(res int) {
-	if res != 200 {
-		log.Fatalln("Request failed with status code:", res)
-	}
+func handleScrape(c echo.Context) error {
+	defer os.Remove(filename)
+	job := strings.ToLower(scraper.CleanString(c.FormValue("job")))
+	scraper.Scrape(job)
+	return c.Attachment(filename, job+"_"+filename)
 }
